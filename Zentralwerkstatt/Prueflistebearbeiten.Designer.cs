0e7e41c803fac9aa121571651ad75091f494e4dd
Florian <Florian@FLOSPC>
2017-03-14 00:00:20 +0100

    Prüfliste bearbeiten Proto

--- Zentralwerkstatt/Prueflistebearbeiten.Designer.cs
+++ Zentralwerkstatt/Prueflistebearbeiten.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Zentralwerkstatt
 {
-    partial class Pruefliste
+    partial class Prueflistebearbeiten
     {
         /// <summary>
         /// Required designer variable.
@@ -29,37 +29,22 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.Prüfliste = new System.Windows.Forms.DataGridView();
             this.Gerätetypen = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Prüfliste = new System.Windows.Forms.DataGridView();
             this.projektzDataSet = new Zentralwerkstatt.projektzDataSet();
+            this.prüfkriterienBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.prüfkriterienTableAdapter = new Zentralwerkstatt.projektzDataSetTableAdapters.prüfkriterienTableAdapter();
+            this.textDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anzeigeartDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.gerätetypenBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.gerätetypenTableAdapter = new Zentralwerkstatt.projektzDataSetTableAdapters.gerätetypenTableAdapter();
-            this.iDGerÃtetypDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bezeichnungDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Prueftext = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Anzeigeart = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.Prüfliste)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Gerätetypen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Prüfliste)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.projektzDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prüfkriterienBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gerätetypenBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Prüfliste
-            // 
-            this.Prüfliste.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Prüfliste.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Prüfliste.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Prueftext,
-            this.Anzeigeart});
-            this.Prüfliste.Location = new System.Drawing.Point(191, 12);
-            this.Prüfliste.Name = "Prüfliste";
-            this.Prüfliste.RowHeadersVisible = false;
-            this.Prüfliste.Size = new System.Drawing.Size(909, 563);
-            this.Prüfliste.TabIndex = 0;
             // 
             // Gerätetypen
             // 
@@ -70,40 +55,59 @@
             this.Gerätetypen.AutoGenerateColumns = false;
             this.Gerätetypen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Gerätetypen.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.iDGerÃtetypDataGridViewTextBoxColumn,
             this.bezeichnungDataGridViewTextBoxColumn});
             this.Gerätetypen.DataSource = this.gerätetypenBindingSource;
-            this.Gerätetypen.Location = new System.Drawing.Point(12, 12);
+            this.Gerätetypen.Location = new System.Drawing.Point(8, 12);
             this.Gerätetypen.Name = "Gerätetypen";
             this.Gerätetypen.ReadOnly = true;
             this.Gerätetypen.RowHeadersVisible = false;
-            this.Gerätetypen.Size = new System.Drawing.Size(173, 563);
-            this.Gerätetypen.TabIndex = 1;
+            this.Gerätetypen.Size = new System.Drawing.Size(173, 557);
+            this.Gerätetypen.TabIndex = 3;
             // 
-            // button1
+            // Prüfliste
             // 
-            this.button1.Location = new System.Drawing.Point(12, 581);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Übernehmen";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(928, 581);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(172, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Abbrechen";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.Prüfliste.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Prüfliste.AutoGenerateColumns = false;
+            this.Prüfliste.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Prüfliste.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.textDataGridViewTextBoxColumn,
+            this.anzeigeartDataGridViewTextBoxColumn});
+            this.Prüfliste.DataSource = this.prüfkriterienBindingSource;
+            this.Prüfliste.Location = new System.Drawing.Point(187, 12);
+            this.Prüfliste.Name = "Prüfliste";
+            this.Prüfliste.RowHeadersVisible = false;
+            this.Prüfliste.Size = new System.Drawing.Size(1048, 557);
+            this.Prüfliste.TabIndex = 2;
             // 
             // projektzDataSet
             // 
             this.projektzDataSet.DataSetName = "projektzDataSet";
             this.projektzDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // prüfkriterienBindingSource
+            // 
+            this.prüfkriterienBindingSource.DataMember = "prüfkriterien";
+            this.prüfkriterienBindingSource.DataSource = this.projektzDataSet;
+            // 
+            // prüfkriterienTableAdapter
+            // 
+            this.prüfkriterienTableAdapter.ClearBeforeFill = true;
+            // 
+            // textDataGridViewTextBoxColumn
+            // 
+            this.textDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.textDataGridViewTextBoxColumn.DataPropertyName = "Text";
+            this.textDataGridViewTextBoxColumn.FillWeight = 10000F;
+            this.textDataGridViewTextBoxColumn.HeaderText = "Text";
+            this.textDataGridViewTextBoxColumn.Name = "textDataGridViewTextBoxColumn";
+            // 
+            // anzeigeartDataGridViewTextBoxColumn
+            // 
+            this.anzeigeartDataGridViewTextBoxColumn.DataPropertyName = "Anzeigeart";
+            this.anzeigeartDataGridViewTextBoxColumn.HeaderText = "Anzeigeart";
+            this.anzeigeartDataGridViewTextBoxColumn.Name = "anzeigeartDataGridViewTextBoxColumn";
             // 
             // gerätetypenBindingSource
             // 
@@ -114,14 +118,6 @@
             // 
             this.gerätetypenTableAdapter.ClearBeforeFill = true;
             // 
-            // iDGerÃtetypDataGridViewTextBoxColumn
-            // 
-            this.iDGerÃtetypDataGridViewTextBoxColumn.DataPropertyName = "IDGerÃ¤tetyp";
-            this.iDGerÃtetypDataGridViewTextBoxColumn.HeaderText = "IDGerÃ¤tetyp";
-            this.iDGerÃtetypDataGridViewTextBoxColumn.Name = "iDGerÃtetypDataGridViewTextBoxColumn";
-            this.iDGerÃtetypDataGridViewTextBoxColumn.ReadOnly = true;
-            this.iDGerÃtetypDataGridViewTextBoxColumn.Visible = false;
-            // 
             // bezeichnungDataGridViewTextBoxColumn
             // 
             this.bezeichnungDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
@@ -131,38 +127,20 @@
             this.bezeichnungDataGridViewTextBoxColumn.Name = "bezeichnungDataGridViewTextBoxColumn";
             this.bezeichnungDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // Prueftext
-            // 
-            this.Prueftext.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Prueftext.FillWeight = 10000F;
-            this.Prueftext.HeaderText = "Prüftext";
-            this.Prueftext.Name = "Prueftext";
-            // 
-            // Anzeigeart
-            // 
-            this.Anzeigeart.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
-            this.Anzeigeart.HeaderText = "Anzeigeart";
-            this.Anzeigeart.Items.AddRange(new object[] {
-            "h",
-            "b",
-            "mm"});
-            this.Anzeigeart.Name = "Anzeigeart";
-            // 
-            // Pruefliste
+            // Prueflistebearbeiten
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 620);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1247, 581);
             this.Controls.Add(this.Gerätetypen);
             this.Controls.Add(this.Prüfliste);
-            this.Name = "Pruefliste";
-            this.Text = "Prüfliste erstellen";
-            this.Load += new System.EventHandler(this.Pruefliste_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.Prüfliste)).EndInit();
+            this.Name = "Prueflistebearbeiten";
+            this.Text = "Prueflistebearbeiten";
+            this.Load += new System.EventHandler(this.Prueflistebearbeiten_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Gerätetypen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Prüfliste)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.projektzDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prüfkriterienBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gerätetypenBindingSource)).EndInit();
             this.ResumeLayout(false);
 
@@ -170,16 +148,15 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView Prüfliste;
         private System.Windows.Forms.DataGridView Gerätetypen;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView Prüfliste;
         private projektzDataSet projektzDataSet;
+        private System.Windows.Forms.BindingSource prüfkriterienBindingSource;
+        private projektzDataSetTableAdapters.prüfkriterienTableAdapter prüfkriterienTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn anzeigeartDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource gerätetypenBindingSource;
         private projektzDataSetTableAdapters.gerätetypenTableAdapter gerätetypenTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn iDGerÃtetypDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn bezeichnungDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Prueftext;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Anzeigeart;
     }
 }
---
+++ Zentralwerkstatt/Prueflistebearbeiten.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zentralwerkstatt
+{
+    public partial class Prueflistebearbeiten : Form
+    {
+        public Prueflistebearbeiten()
+        {
+            InitializeComponent();
+        }
+
+        private void Prueflistebearbeiten_Load(object sender, EventArgs e)
+        {
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "projektzDataSet.gerätetypen". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.gerätetypenTableAdapter.Fill(this.projektzDataSet.gerätetypen);
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "projektzDataSet.prüfkriterien". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.prüfkriterienTableAdapter.Fill(this.projektzDataSet.prüfkriterien);
+
+        }
+    }
+}
